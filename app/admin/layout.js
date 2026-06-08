ed91431bc58fa083cf70ec3c2d2818579f28f973
AmiraaHesham <amira.hesham.fci@gmail.com>
2026-06-08 23:55:44 +0300

    refactor: improve layout structure by removing unnecessary height constraints

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -23,22 +23,21 @@ export default function AdminLayout({ children }) {
       <div className="flex flex-col gap-10 justify-center items-center h-screen bg-color1">
         <Image src="/Images/logo.png" alt="" priority width={200} height={200} className="w-[100px] h-[100px]  border-t-transparent rounded-full animate-pulse" />
         <h1 className="md:text-5xl xs:text-4xl  font-serif font-semibold animate-pulse bg-gradient-to-r from-[#d62828] via-color3 to-color2 bg-clip-text text-transparent "></h1>
-
       </div>
 
     );
   }
 
   return (
-    <div className=" h-screen bg-[#F9FAFB]">
+    <div className=" bg-[#F9FAFB]">
       {
         role === 'ADMIN' ?
           (
-            <main className=" flex border h-screen ">
+            <main className=" flex border ">
               <SideMenu homepage={"bg-red-100 text-red-500"} />
               <div className="w-full h-full ">
                 <Header page_title={"Homepage Management"} />
-                <div className="bg-[#F9FAFB] h-screen ">{children}</div>
+                <div className="bg-[#F9FAFB] h-auto ">{children}</div>
               </div>
             </main>
           ) : (
